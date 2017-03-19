88eb7090b621e17ed0b9c4ef168e34a2df422dda
kimnorth <benignhumour@gmail.com>
2017-03-19 21:16:06 +0000

    Nearly at MVP. Can now add words to the list of main words. Need to make it so the new word is added at the top of the list. Should probably write a new method in my wordList class that adds it to the start of the index. Could maybe make it so the arraylist is reversed from the start. Also need to add other CRUD actions.

--- Wordy/app/src/main/java/com/codeclan/wordyapp/MainActivity.java
+++ Wordy/app/src/main/java/com/codeclan/wordyapp/MainActivity.java
@@ -13,6 +13,7 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     Button learnButton;
+    Button newWordButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,28 +21,51 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        learnButton = (Button)findViewById(R.id.learn_button);
+        learnButton = (Button)findViewById(R.id.learn_button);
+        newWordButton = (Button)findViewById(R.id.new_word_button);
 
         WordList wordList = new WordList();
-        ArrayList<Word> mainListWords = wordList.getWords();
 
-        mainListWords.add(new Word("banana", "skin"));
-        mainListWords.add(new Word("cheese", "A yellow kind of beef"));
-        mainListWords.add(new Word("Godalming", "a place in surrey"));
-        mainListWords.add(new Word("Gland", "something rude"));
-        mainListWords.add(new Word("Sick", "excellent"));
-        mainListWords.add(new Word("Dog", "a dyslexic god"));
+        wordList.addWord(new Word("banana", "skin"));
+        wordList.addWord(new Word("cheese", "A yellow kind of beefzzzzzzzzzzzzzzA yellow kind of beefzzzzzzzzzzzzzzA yellow kind of beefzzzzzzzzzzzzzzA yellow kind of beefzzzzzzzzzzzzzzA yellow kind of beefzzzzzzzzzzzzzzA yellow kind of beefzzzzzzzzzzzzzzA yellow kind of beefzzzzzzzzzzzzzzA yellow kind of beefzzzzzzzzzzzzzzA yellow kind of beefzzzzzzzzzzzzzzA yellow kind of beefzzzzzzzzzzzzzzA yellow kind of beefzzzzzzzzzzzzzzA yellow kind of beefzzzzzzzzzzzzzz"));
+        wordList.addWord(new Word("Godalming", "a place in surrey"));
+        wordList.addWord(new Word("Gland", "something rude"));
+        wordList.addWord(new Word("Sick", "excellent"));
+        wordList.addWord(new Word("Dog", "a dyslexic god"));
+        wordList.addWord(new Word("Dog", "a dyslexic god"));
+        wordList.addWord(new Word("banana", "skin"));
+        wordList.addWord(new Word("banana", "skin"));
+        wordList.addWord(new Word("banana", "skin"));
+        wordList.addWord(new Word("Dog", "a dyslexic god"));
+
+        Intent newWordIntent = getIntent();
+        Bundle extras = newWordIntent.getExtras();
+
+        Word newWord = new Word(extras.getString("new_word"), extras.getString("new_definition"));
+        if (newWord != null){
+            wordList.addWord(newWord);
+        }
+
+        ArrayList<Word> mainListWords = wordList.getWords();
 
         WordListAdapter wordListAdapter = new WordListAdapter(this, mainListWords);
 
         ListView listView = (ListView) findViewById(R.id.main_list);
         listView.setAdapter(wordListAdapter);
 
+
     }
 
     public void onClickLearnButton(View button){
         Log.d(getClass().toString(), "Button was clicked");
         Intent intent = new Intent(this, MemoryGameActivity.class);
+        startActivity(intent);
+    }
+
+    public void onClickNewWordButton(View button){
+
+        Log.d(getClass().toString(), "Button was clicked");
+        Intent intent = new Intent(this, NewWordActivity.class);
         startActivity(intent);
     }
 }
--- Wordy/app/src/main/java/com/codeclan/wordyapp/NewWordActivity.java
+++ Wordy/app/src/main/java/com/codeclan/wordyapp/NewWordActivity.java
@@ -3,15 +3,42 @@ package com.codeclan.wordyapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class NewWordActivity extends AppCompatActivity {
+
+    TextView enterNewWord;
+    TextView enterNewDefinition;
+    EditText fieldNewWord;
+    EditText fieldNewDefinition;
+    Button addWord;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_word);
 
-        Intent intent = getIntent();
-//        Bundle extras = intent.getExtras();
+        enterNewWord = (TextView)findViewById(R.id.enter_new_word);
+        enterNewDefinition = (TextView)findViewById(R.id.enter_new_definition);
+        fieldNewWord = (EditText)findViewById(R.id.new_word_name);
+        fieldNewDefinition = (EditText)findViewById(R.id.new_word_definition);
+        addWord = (Button)findViewById(R.id.add_word_button);
+
+    }
+
+    public void onClickAddWordButton(View button){
+
+        String newWord = fieldNewWord.getText().toString();
+        String newDefinition = fieldNewDefinition.getText().toString();
+
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("new_word", newWord);
+        intent.putExtra("new_definition", newDefinition);
+        startActivity(intent);
     }
 }
