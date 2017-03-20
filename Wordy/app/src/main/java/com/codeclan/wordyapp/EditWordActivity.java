65d8f404d9454ae4b2c16927590bbbef39f65a5b
kimnorth <benignhumour@gmail.com>
2017-03-20 14:53:11 +0000

    My edit word screen now allows you to edit the word, but instead of overwriting the wordlist object to shared preferences, it is just adding an extra word onto the list.

--- Wordy/app/src/main/java/com/codeclan/wordyapp/EditWordActivity.java
+++ Wordy/app/src/main/java/com/codeclan/wordyapp/EditWordActivity.java
@@ -1,17 +1,28 @@
 package com.codeclan.wordyapp;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
 public class EditWordActivity extends AppCompatActivity {
 
-    private Word word;
+    public static final String WORDLIST = "WordList";
+
+    private Word bundledWord;
     private String passedWord;
     private String passedDefinition;
     private EditText wordToEdit;
     private EditText definitionToEdit;
+    private Button editWordButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,16 +30,48 @@ public class EditWordActivity extends AppCompatActivity {
         setContentView(R.layout.activity_edit_word);
 
         Bundle bundle = getIntent().getExtras();
-        word = (Word) bundle.getSerializable("word");
+        bundledWord = (Word) bundle.getSerializable("word");
 
-        System.out.println(word.getWord());
-        System.out.println(word.getDefintion());
+        System.out.println(bundledWord.getWord());
+        System.out.println(bundledWord.getDefintion());
 
         wordToEdit = (EditText)findViewById(R.id.word_to_edit);
-        wordToEdit.setText(word.getWord(), TextView.BufferType.EDITABLE);
+        wordToEdit.setText(bundledWord.getWord(), TextView.BufferType.EDITABLE);
 
         definitionToEdit = (EditText)findViewById(R.id.definition_to_edit);
-        definitionToEdit.setText(word.getDefintion(), TextView.BufferType.EDITABLE);
+        definitionToEdit.setText(bundledWord.getDefintion(), TextView.BufferType.EDITABLE);
+
+        editWordButton = (Button)findViewById(R.id.edit_word_button);
+
+    }
+
+    public void onClickUpdateButton(View button){
+
+        String editedWord = wordToEdit.getText().toString();
+        String editedDefinition = definitionToEdit.getText().toString();
+
+        SharedPreferences sharedPref = getSharedPreferences(WORDLIST, Context.MODE_PRIVATE);
+        String myWordList = sharedPref.getString("WordList", "This message shouldn't come up");
+
+        Gson gson = new Gson();
+
+        TypeToken<WordList> wordListObject = new TypeToken<WordList>(){};
+        WordList wordList = gson.fromJson(myWordList, wordListObject.getType());
+
+        // I could either edit the wordlist object containing the object, or delete the word and add
+        // a new one, so it sits at the top of the list. I'm going to try the latter.
+
+        wordList.deleteWord(bundledWord);
+        Word word = new Word(editedWord, editedDefinition);
+        wordList.addWord(word);
+
+        SharedPreferences.Editor editor = sharedPref.edit();
+
+        editor.putString( "WordList", gson.toJson( wordList ));
+        editor.apply();
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
 
     }
 
